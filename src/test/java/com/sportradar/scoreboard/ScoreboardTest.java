b2634cbb95b10e1756e95df3609145338f76aae1
Arjun Snehaj Lal <arjun.s.lal@gmail.com>
2025-05-16 08:42:14 +0200

    Implement most testcases
    Implement all of the testcases other than ordering of the summary

--- src/test/java/com/sportradar/scoreboard/ScoreboardTest.java
+++ src/test/java/com/sportradar/scoreboard/ScoreboardTest.java
@@ -7,30 +7,44 @@ package com.sportradar.scoreboard;
 /**
  * Test class for the Scoreboard system using JUnit 5.
  */
+import java.util.*;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
 public class ScoreboardTest {
 
+    private Scoreboard scoreboard;
+
     //Initialize tests
     @Before
     public void init() {
-    }
-    //Basic Testcases
+        scoreboard = new Scoreboard();
 
+    }
+    
+    //Basic Testcases
     //Test that a match can be started
     @Test
     public void testStartMatch() {
+        scoreboard.startMatch("Mexico", "Canada");
+        assertEquals(1, scoreboard.getSummary().size());
     }
 
     //Test that score can be updated
     @Test
     public void testUpdateScore() {
+        scoreboard.startMatch("Germany", "France");
+        scoreboard.updateScore("Germany", "France", 2, 2);
+        assertTrue(scoreboard.getSummary().contains("Germany 2 - France 2"));
     }
 
     //Test that a match can be ended
     @Test
     public void testFinishMatch() {
+        scoreboard.startMatch("Italy", "Portugal");
+        scoreboard.finishMatch("Italy", "Portugal");
+        assertEquals(0, scoreboard.getSummary().size());
     }
 
     //Test that a the scores are sorted in descending order and that the most recent match will be higher in the order if the total score is the same
@@ -43,16 +57,22 @@ public class ScoreboardTest {
     //Test that startMatch()cannot be called on an existing match 
     @Test
     public void testStartDuplicateMatchThrows() {
+        scoreboard.startMatch("Spain", "Brazil");
+        assertThrows(IllegalStateException.class, () -> scoreboard.startMatch("Spain", "Brazil"));
     }
 
     //Test that a match has to exist before updating the scores
     @Test
     public void testUpdateNonExistentMatchThrows() {
-    }
-    //Test that a match has to exist before finishing
+        assertThrows(NoSuchElementException.class, () -> scoreboard.updateScore("Germany", "Brazil", 1, 1));
 
+    }
+
+    //Test that a match has to exist before finishing
     @Test
     public void testFinishNonExistentMatchThrows() {
+        assertThrows(NoSuchElementException.class, () -> scoreboard.finishMatch("Spain", "Portugal"));
+
     }
 
     //Test in case of tied score(total), that the recently started one will be higher up(TODO: redundant?)
@@ -63,10 +83,15 @@ public class ScoreboardTest {
     //Test that a team cannot play itself
     @Test
     public void testSameTeamNamesThrowsException() {
+        assertThrows(IllegalArgumentException.class, () -> scoreboard.startMatch("Argentina", "Argentina"));
+
     }
 
     //Test that a match cannot be started with a team that is already in a match
     @Test
     public void testTeamCannotBeInTwoMatches() {
+        scoreboard.startMatch("Spain", "Brazil");
+        assertThrows(IllegalStateException.class, () -> scoreboard.startMatch("Spain", "Germany"));
+        assertThrows(IllegalStateException.class, () -> scoreboard.startMatch("France", "Brazil"));
     }
 }
